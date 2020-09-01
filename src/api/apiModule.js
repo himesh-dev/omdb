fe0fab3af0bbe2421c223c5cfbdb551fbdd88adb
himesh-dev <askhimesh1@gmail.com>
2020-09-02 05:11:49 +0530

    http--> https

--- src/api/apiModule.js
+++ src/api/apiModule.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://www.omdbapi.com/?apikey=4ef68d";
+const BASE_URL = "https://www.omdbapi.com/?apikey=4ef68d";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
